a51d844c642fa4e5bd7968e724e9fc78d3c8a82d
Ceneses <wo51study@163.com>
2020-01-09 21:26:49 +0800

    添加videoDemo

---
+++ videoDemo/pages/index/index.js
@@ -0,0 +1,87 @@
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        list:[
+            {
+                id:"1",
+                title:"第1个视频",
+                // videoUrl:""
+            }, 
+            {
+                id: "2",
+                title: "第2个视频",
+                // videoUrl:""
+            },
+            {
+                id: "3",
+                title: "第3个视频",
+                // videoUrl:""
+            },
+            {
+                id: "4",
+                title: "第4个视频",
+                // videoUrl:""
+            }
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
